5f6af5a74163ff31ff6dfa777e316fe9f5c8bd51
Ritik Kumar Pathak <phritik06@gmail.com>
2024-09-25 13:41:09 +0530

    basic setup

---
+++ src/components/client/LandingPage/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-blue-300 py-12">
+      <div className=" container mx-auto px-4 text-center text-gray-600">
+        <h1>All Copyrights reserved @scheduleeasy.com</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/client/LandingPage/Header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Logo from "../../../assets/logo.png";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "@radix-ui/react-icons";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="mx-auto py-2 px-4 flex justify-between shadow-amber-500 shadow-lg border-b-2">
+        <Link href={"/"} className=" flex items-center">
+          <Image src={Logo} height={100} width={100} alt="logo" />
+        </Link>
+        <div className=" flex items-center gap-3">
+          <Link href={`/events?create=true`}>
+            <Button className=" flex items-center gap-2">
+              <PlusIcon className=" h-4 w-4" />
+              Create Event
+            </Button>
+          </Link>
+          <Button variant={"outline"}>Login</Button>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
